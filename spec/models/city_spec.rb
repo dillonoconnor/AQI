a26a91742b3e62433dd059c371334c7e7164061c
Dillon O'Connor <dilong818@gmail.com>
2021-01-27 14:30:18 -0500

    add specs for City model

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#fetch_aqi" do
+    it "returns a city object" do
+      c = City.fetch_aqi("Tianjin")
+      expect(c).to be_a(Hash)
+      expect(c["city"]["name"]).to eq("Tianjin (天津)")
+    end
+  end
+
+  describe ".recent?" do
+    it "returns false for database entries older than 1 day" do
+      c = City.create!(name:"testcity", created_at: 2.days.ago)
+      expect(c.recent?).not_to be_truthy
+    end
+    it "returns true for database entries >= 1 day old" do
+      c = City.create!(name:"testcity", created_at: 23.hours.ago + 59.minutes)
+      expect(c.recent?).to be_truthy
+      c["created_at"] = 1.hour.ago
+      expect(c.recent?).to be_truthy
+    end
+  end
 end
